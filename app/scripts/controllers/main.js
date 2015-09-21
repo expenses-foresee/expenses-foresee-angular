5a17bc113799bc92be4d65b8b5531a3aaffda047
Spart Chou <spart.chou@gmail.com>
2015-09-21 13:18:09 +0800

    scaffold with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name expensesForeseeAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the expensesForeseeAngularApp
+ */
+angular.module('expensesForeseeAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
